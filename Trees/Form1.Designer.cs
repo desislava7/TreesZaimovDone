22437f02c9106a1d4bbffbde4fae9ed0ea43e488
desislava7 <desislavadiimitrova767@gmail.com>
2024-04-30 11:34:05 +0300

    do insert

---
+++ Trees/Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace Trees
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.cmb1 = new System.Windows.Forms.ComboBox();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.cmb2 = new System.Windows.Forms.ComboBox();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.cmb3 = new System.Windows.Forms.ComboBox();
+            this.lbl5 = new System.Windows.Forms.Label();
+            this.cmb4 = new System.Windows.Forms.ComboBox();
+            this.lbl6 = new System.Windows.Forms.Label();
+            this.cmb5 = new System.Windows.Forms.ComboBox();
+            this.lbl7 = new System.Windows.Forms.Label();
+            this.cmb6 = new System.Windows.Forms.ComboBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.lbl8 = new System.Windows.Forms.Label();
+            this.lbl9 = new System.Windows.Forms.Label();
+            this.lbl10 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(40, 19);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(75, 13);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "Naimenovanie";
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(29, 35);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(121, 20);
+            this.txt1.TabIndex = 1;
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(40, 58);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(43, 13);
+            this.lbl2.TabIndex = 2;
+            this.lbl2.Text = "OTDEL";
+            // 
+            // cmb1
+            // 
+            this.cmb1.FormattingEnabled = true;
+            this.cmb1.Location = new System.Drawing.Point(29, 74);
+            this.cmb1.Name = "cmb1";
+            this.cmb1.Size = new System.Drawing.Size(121, 21);
+            this.cmb1.TabIndex = 3;
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Location = new System.Drawing.Point(40, 98);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(34, 13);
+            this.lbl3.TabIndex = 4;
+            this.lbl3.Text = "CLAS";
+            // 
+            // cmb2
+            // 
+            this.cmb2.FormattingEnabled = true;
+            this.cmb2.Location = new System.Drawing.Point(29, 114);
+            this.cmb2.Name = "cmb2";
+            this.cmb2.Size = new System.Drawing.Size(121, 21);
+            this.cmb2.TabIndex = 5;
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.Location = new System.Drawing.Point(40, 149);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(41, 13);
+            this.lbl4.TabIndex = 6;
+            this.lbl4.Text = "Razred";
+            // 
+            // cmb3
+            // 
+            this.cmb3.FormattingEnabled = true;
+            this.cmb3.Location = new System.Drawing.Point(29, 165);
+            this.cmb3.Name = "cmb3";
+            this.cmb3.Size = new System.Drawing.Size(121, 21);
+            this.cmb3.TabIndex = 7;
+            // 
+            // lbl5
+            // 
+            this.lbl5.AutoSize = true;
+            this.lbl5.Location = new System.Drawing.Point(43, 193);
+            this.lbl5.Name = "lbl5";
+            this.lbl5.Size = new System.Drawing.Size(36, 13);
+            this.lbl5.TabIndex = 8;
+            this.lbl5.Text = "Family";
+            // 
+            // cmb4
+            // 
+            this.cmb4.FormattingEnabled = true;
+            this.cmb4.Location = new System.Drawing.Point(29, 210);
+            this.cmb4.Name = "cmb4";
+            this.cmb4.Size = new System.Drawing.Size(121, 21);
+            this.cmb4.TabIndex = 9;
+            // 
+            // lbl6
+            // 
+            this.lbl6.AutoSize = true;
+            this.lbl6.Location = new System.Drawing.Point(43, 238);
+            this.lbl6.Name = "lbl6";
+            this.lbl6.Size = new System.Drawing.Size(27, 13);
+            this.lbl6.TabIndex = 10;
+            this.lbl6.Text = "Rod";
+            // 
+            // cmb5
+            // 
+            this.cmb5.FormattingEnabled = true;
+            this.cmb5.Location = new System.Drawing.Point(29, 254);
+            this.cmb5.Name = "cmb5";
+            this.cmb5.Size = new System.Drawing.Size(121, 21);
+            this.cmb5.TabIndex = 11;
+            // 
+            // lbl7
+            // 
+            this.lbl7.AutoSize = true;
+            this.lbl7.Location = new System.Drawing.Point(43, 282);
+            this.lbl7.Name = "lbl7";
+            this.lbl7.Size = new System.Drawing.Size(31, 13);
+            this.lbl7.TabIndex = 12;
+            this.lbl7.Text = "Type";
+            // 
+            // cmb6
+            // 
+            this.cmb6.FormattingEnabled = true;
+            this.cmb6.Location = new System.Drawing.Point(29, 299);
+            this.cmb6.Name = "cmb6";
+            this.cmb6.Size = new System.Drawing.Size(121, 21);
+            this.cmb6.TabIndex = 13;
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(206, 34);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(47, 20);
+            this.txt2.TabIndex = 14;
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(193, 85);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(137, 39);
+            this.btn1.TabIndex = 15;
+            this.btn1.Text = "Add new tree";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(193, 156);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(137, 37);
+            this.btn2.TabIndex = 16;
+            this.btn2.Text = "Update";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(339, 335);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(277, 20);
+            this.textBox3.TabIndex = 17;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.pictureBox1.Location = new System.Drawing.Point(606, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(155, 206);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 19;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txt3
+            // 
+            this.txt3.Location = new System.Drawing.Point(386, 52);
+            this.txt3.Multiline = true;
+            this.txt3.Name = "txt3";
+            this.txt3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txt3.Size = new System.Drawing.Size(173, 243);
+            this.txt3.TabIndex = 20;
+            // 
+            // lbl8
+            // 
+            this.lbl8.AutoSize = true;
+            this.lbl8.Location = new System.Drawing.Point(203, 9);
+            this.lbl8.Name = "lbl8";
+            this.lbl8.Size = new System.Drawing.Size(18, 13);
+            this.lbl8.TabIndex = 21;
+            this.lbl8.Text = "ID";
+            // 
+            // lbl9
+            // 
+            this.lbl9.AutoSize = true;
+            this.lbl9.Location = new System.Drawing.Point(353, 316);
+            this.lbl9.Name = "lbl9";
+            this.lbl9.Size = new System.Drawing.Size(83, 13);
+            this.lbl9.TabIndex = 22;
+            this.lbl9.Text = "Add Image URL";
+            // 
+            // lbl10
+            // 
+            this.lbl10.AutoSize = true;
+            this.lbl10.Location = new System.Drawing.Point(386, 19);
+            this.lbl10.Name = "lbl10";
+            this.lbl10.Size = new System.Drawing.Size(56, 13);
+            this.lbl10.TabIndex = 23;
+            this.lbl10.Text = "Discription";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl10);
+            this.Controls.Add(this.lbl9);
+            this.Controls.Add(this.lbl8);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.cmb6);
+            this.Controls.Add(this.lbl7);
+            this.Controls.Add(this.cmb5);
+            this.Controls.Add(this.lbl6);
+            this.Controls.Add(this.cmb4);
+            this.Controls.Add(this.lbl5);
+            this.Controls.Add(this.cmb3);
+            this.Controls.Add(this.lbl4);
+            this.Controls.Add(this.cmb2);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.cmb1);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.lbl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.ComboBox cmb1;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.ComboBox cmb2;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.ComboBox cmb3;
+        private System.Windows.Forms.Label lbl5;
+        private System.Windows.Forms.ComboBox cmb4;
+        private System.Windows.Forms.Label lbl6;
+        private System.Windows.Forms.ComboBox cmb5;
+        private System.Windows.Forms.Label lbl7;
+        private System.Windows.Forms.ComboBox cmb6;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.Label lbl8;
+        private System.Windows.Forms.Label lbl9;
+        private System.Windows.Forms.Label lbl10;
+    }
+}
+
---
+++ Trees/Form1.cs
@@ -0,0 +1,192 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Trees
+{
+    public partial class Form1 : Form
+    {
+        string connstr = "server = 10.6.0.33 ;" +
+            "port =3306;" +
+            "user =PC1;" +
+            "password= 1111;" +
+            "database=trees_zaimov";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MySqlConnection connect = new MySqlConnection(connstr);
+            if(connect.State==0) connect.Open();
+            MessageBox.Show("Connection NOW opened");
+            
+
+            MySqlCommand query = new MySqlCommand("Select * from OTDEL",connect);
+            MySqlDataReader readerCombo = query.ExecuteReader();
+            List<ComboBoxItem> items1 = new List<ComboBoxItem>();
+
+            while (readerCombo.Read())
+            {
+                ComboBoxItem item = new ComboBoxItem();
+                item.Text = readerCombo[1].ToString();
+                item.Value = (int)readerCombo[0];
+
+                items1.Add(item);
+            }
+            readerCombo.Close();
+            cmb1.DataSource = items1;
+            cmb1.DisplayMember = "Text";
+            cmb1.ValueMember = "Value";
+
+            //
+            List<ComboBoxItem> items2 = new List<ComboBoxItem>();
+            query = new MySqlCommand("Select * from Class", connect);
+             readerCombo = query.ExecuteReader();
+             
+
+            while (readerCombo.Read())
+            {
+                ComboBoxItem item2 = new ComboBoxItem();
+                item2.Text = readerCombo[1].ToString();
+                item2.Value = (int)readerCombo[0];
+
+                items2.Add(item2);
+            }
+            readerCombo.Close();
+            cmb2.DataSource = items2;
+            cmb2.DisplayMember = "Text";
+            cmb2.ValueMember = "Value";
+
+
+
+
+            //
+            List<ComboBoxItem> items3 = new List<ComboBoxItem>();
+            query = new MySqlCommand("Select * from Razred", connect);
+            readerCombo = query.ExecuteReader();
+            
+
+            while (readerCombo.Read())
+            {
+                ComboBoxItem item3 = new ComboBoxItem();
+                item3.Text = readerCombo[1].ToString();
+                item3.Value = (int)readerCombo[0];
+
+                items3.Add(item3);
+            }
+            readerCombo.Close();
+            cmb3.DataSource = items3;
+            cmb3.DisplayMember = "Text";
+            cmb3.ValueMember = "Value";
+
+            //
+            List<ComboBoxItem> items4 = new List<ComboBoxItem>();
+            query = new MySqlCommand("Select * from Family", connect);
+            readerCombo = query.ExecuteReader();
+           
+
+            while (readerCombo.Read())
+            {
+                ComboBoxItem item4 = new ComboBoxItem();
+                item4.Text = readerCombo[1].ToString();
+                item4.Value = (int)readerCombo[0];
+
+                items4.Add(item4);
+            }
+            readerCombo.Close();
+            cmb4.DataSource = items4;
+            cmb4.DisplayMember = "Text";
+            cmb4.ValueMember = "Value";
+
+
+
+            //
+            List<ComboBoxItem> items5 = new List<ComboBoxItem>();
+            query = new MySqlCommand("Select * from Rod ", connect);
+            readerCombo = query.ExecuteReader();
+            
+
+            while (readerCombo.Read())
+            {
+                ComboBoxItem item5 = new ComboBoxItem();
+                item5.Text = readerCombo[1].ToString();
+                item5.Value = (int)readerCombo[0];
+
+                items5.Add(item5);
+            }
+            readerCombo.Close();
+            cmb5.DataSource = items5;
+            cmb5.DisplayMember = "Text";
+            cmb5.ValueMember = "Value";
+
+
+
+
+            //
+            List<ComboBoxItem> items6 = new List<ComboBoxItem>();
+            query = new MySqlCommand("Select * from type", connect);
+            readerCombo = query.ExecuteReader();
+           
+
+            while (readerCombo.Read())
+            {
+                ComboBoxItem item6 = new ComboBoxItem();
+                item6.Text = readerCombo[1].ToString();
+                item6.Value = (int)readerCombo[0];
+
+                items6.Add(item6);
+            }
+            readerCombo.Close();
+            cmb6.DataSource = items6;
+            cmb6.DisplayMember = "Text";
+            cmb6.ValueMember = "Value";
+            connect.Close();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            string sql = "INSERT INTO `trees_zaimov`.`tree`" +
+                "(`name`,imageUrl," +
+                "otdel_id,class_id,razred_id," +
+                "family_id,rod_id,vid_id, " +
+                "description,DateLastUpdate)" +
+                "VALUES(@name,@img,@otdel,@class," +
+                "@razred,@family,@rod,@vid,@info,@dateReg);";
+
+            MySqlConnection connect = new MySqlConnection(connstr);
+            if(connect.State==0) connect.Open();
+
+            MySqlCommand query = new MySqlCommand(sql, connect);
+
+            query.Parameters.AddWithValue("@name", txt1.Text);
+            query.Parameters.AddWithValue("@img", txt2.Text);
+            query.Parameters.AddWithValue("@otdel", cmb1.SelectedValue);
+            query.Parameters.AddWithValue("@class", cmb2.SelectedValue);
+            query.Parameters.AddWithValue("@razred", cmb3.SelectedValue);
+            query.Parameters.AddWithValue("@family", cmb4.SelectedValue);
+            query.Parameters.AddWithValue("@rod", cmb5.SelectedValue);
+            query.Parameters.AddWithValue("@vid", cmb6.SelectedValue);
+            query.Parameters.AddWithValue("@info", txt3.Text);
+            query.Parameters.AddWithValue("@dateReg", DateTime.Now);
+
+            query.ExecuteNonQuery();
+            MessageBox.Show("dobavih zapis");
+            connect.Close();
+
+        }
+    }
+}
